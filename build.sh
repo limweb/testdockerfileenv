3c93ff77345d45513bada3ee0e2222c19c439348
lim web <limweb@hotmail.com>
2020-05-27 16:21:59 +0700

    xx

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker rm -f $(docker ps -a -q)
-TEST_ENV=${1:?"AAAAAAA"}
+TEST_ENV=${1:-"AAAAAAA"}
 echo $TEST_ENV
 docker build -t myimage --build-arg TEST_ENV=$TEST_ENV  .
 docker run -d myimage sleep 1d
