f4aea39f26f9bcdc08e8a2570248d712e9694ff3
lim web <limweb@hotmail.com>
2020-05-27 15:19:42 +0700

    3

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 'test env'
 echo $TEST_ENV
-sed 's/$TEST_ENV/${TEST_END}/g;' /var/www/html/.env >  /var/www.html/.env
+sed 's/$TEST_ENV/${TEST_END}/g;' /var/www/html/.env >  /var/www/html/.env
