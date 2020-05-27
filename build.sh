9bd6f95f3e2937c403d25590be0d862be2cf4d9f
lim web <limweb@hotmail.com>
2020-05-27 20:26:25 +0700

    s

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ echo $TEST_ENV
 # docker build -t myimage --build-arg TEST_ENV=$TEST_ENV  .
 docker build -t myimage  .
 # docker run -d -p 80:80 —name mytest -rm  myimage sleep 1d
-docker run -it --rm  -d  -p 80:80 -e TEST_ENV=$TEST_ENV --name mytest myimage sleep 1d
-docker exec -it $(docker ps -a -q)  cat .env
+# docker run -it --rm  -d  -p 80:80 -e TEST_ENV=$TEST_ENV --name mytest myimage sleep 1d
+docker run -it --rm  -d  -p 80:80 -e TEST_ENV=$TEST_ENV --name mytest myimage
+# docker exec -it $(docker ps -a -q)  cat .env
