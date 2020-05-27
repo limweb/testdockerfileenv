8182f5988c0aed8f48a37f2515283ac6af4fb971
lim web <limweb@hotmail.com>
2020-05-27 16:16:11 +0700

    x

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker rm $(docker ps -a -q)
-docker build -t myimage --build-arg TEST_ENV="test"  .
+docker rm -f $(docker ps -a -q)
+TEST_ENV=${1:?"AAAAAAA"}
+echo $TEST_ENV
+docker build -t myimage --build-arg TEST_ENV=$TEST_ENV  .
 docker run -d myimage sleep 1d
 docker image prune -f
 docker exec -it $(docker ps -a -q)  bash 
