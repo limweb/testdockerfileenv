b666fb440b028bf08ff647a2d57abf9c95c28a47
lim web <limweb@hotmail.com>
2020-05-27 15:13:57 +0700

    3

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 chmod +x docker-entrypoint.sh
+docker rm $(docker ps -a -q)
 docker build -t myimage --build-arg TEST_ENV="test"  .
 docker run -d myimage sleep 1d
 docker image prune -f
-docker exec -it {docker ps -q }  bash 
+docker exec -it $(docker ps -a -q)  bash 
